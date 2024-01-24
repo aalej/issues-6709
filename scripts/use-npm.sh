e7636cd04124181a74f15fca3da82185d9e64984
aalej <alejandromarco@google.com>
2024-01-24 19:50:43 +0800

    first commit

--- scripts/use-npm.sh
+++ scripts/use-npm.sh
@@ -2,7 +2,7 @@
 
 echo "-- Starting script"
 
-# Attempt to delete `firebase-tools` installed via NPM.
+# Attempt to delete `firebase-tools` installed via curl.
 if [[ $(which firebase) ]]; then
     echo "-- Found 'firebase-tools' installed, deleting"
     echo "-- Running 'curl -sL firebase.tools | uninstall=true bash'"
@@ -11,7 +11,7 @@ else
     echo "-- Did not find 'firebase-tools'"
 fi
 
-# Install the standalone binary version of `firebase-tools`
+# Install the NPM version of `firebase-tools`
 echo "-- Running 'npm install -g firebase-tools'"
 npm install -g firebase-tools
 
